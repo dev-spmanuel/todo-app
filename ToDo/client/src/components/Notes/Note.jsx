3c740da2a818a21cefdfb14d3851bb97090c03c7
spmanolo <manolosan2001@gmail.com>
2024-10-16 21:17:07 +0200

    Refactor Note component to enable editing of title

--- ToDo/client/src/components/Notes/Note.jsx
+++ ToDo/client/src/components/Notes/Note.jsx
@@ -1,24 +1,73 @@
+import { useState } from "react";
 import ToDos from "./ToDos";
+import EditModeButton from "./EditModeButton";
+import EditTitleButton from "./EditTitleButton";
 
 export default function Note({ note, handleUpdateNote }) {
+
+  const [editMode, setEditMode] = useState(false)
+  const [editTitle, setEditTitle] = useState(false)
+  const [newTitle, setNewTitle] = useState(note.title)
+
+  function handleEditMode() {
+    setEditTitle(false)
+    setEditMode(!editMode)
+  }
+
+  function handleEditTitle() {
+    setEditTitle(!editTitle)
+  }
+
+  function handleSetTitle() {
+    console.log("Changing title...")
+    const updatedNote = { ...note, title: newTitle }
+    handleUpdateNote(updatedNote)
+    setEditTitle(false)
+  }
+
   return (
     <section className="bg-blue-200 rounded-lg shadow-lg px-3 py-2 dark:bg-slate-400">
       <div className="flex justify-between gap-5">
         <div className="flex font-bold">
-          {note.title || "Título"}
+          {editMode && editTitle ? (
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              className="border rounded px-2 py-1"
+            />
+          ) : (
+            note.title
+          )}
+          {editMode && (
+            <div className="flex justify-center">
+              <EditTitleButton
+                editTitle={editTitle}
+                handleEditTitle={handleEditTitle}
+                handleSetTitle={handleSetTitle}
+              />
+            </div>
+          )}
         </div>
-        {/* <div className="flex justify-end">
-          <button className="">
-            Ed
-          </button>
-        </div> */}
+
+        <div className="flex justify-end">
+          <EditModeButton
+            editMode={editMode}
+            handleEditMode={handleEditMode}
+          />
+        </div>
+
       </div>
 
       {/* Separador */}
       <div className="border-b border-gray-600 dark:border-gray-200 my-2"></div>
 
-      <ToDos note={note} updateNote={handleUpdateNote} />
+      <ToDos
+        note={note}
+        updateNote={handleUpdateNote}
+        editMode={editMode}
+      />
 
-    </section>
+    </section >
   )
 }
--- ToDo/client/src/components/Results.jsx
+++ ToDo/client/src/components/Results.jsx
@@ -2,9 +2,6 @@ import { useState } from "react"
 import Note from "./Notes/Note"
 
 export default function Results({ notes, updateNote }) {
-
-  const [editMode, setEditMode] = useState(false)
-
   return (
     <div className="bg-white dark:bg-slate-800 min-h-screen px-16 grid place-content-center gap-8">
       <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
